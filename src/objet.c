55e33520eaa58eba83a9d4d936e97b7594670171
Sacane <johan.ramaroson@gmail.com>
2021-04-04 00:20:36 +0200

    refract

--- src/objet.c
+++ src/objet.c
@@ -2,19 +2,19 @@
 
 
 void remplis_projectile(Objet* obj, Deplacement* deplacement){
-  if(obj->type != LANCEUR){
-    printf("Erreur, l'objet n'est pas un lanceur !\n");
-    return;
-  }
-  obj->donnee_suppl = deplacement;
+    if(obj->type != LANCEUR){
+        printf("Erreur, l'objet n'est pas un lanceur !\n");
+        return;
+    }
+    obj->donnee_suppl = deplacement;
 }
 
 void remplis_lanceur(Objet* lanceur, Generation* gen){
-  if(lanceur->type != PROJECTILE){
-    printf("Erreur, l'objet n'est pas un lanceur !\n");
-    return;
-  }
-  lanceur->donnee_suppl = gen;
+    if(lanceur->type != PROJECTILE){
+      printf("Erreur, l'objet n'est pas un lanceur !\n");
+      return;
+    }
+    lanceur->donnee_suppl = gen;
 }
 
 void verif_malloc(void *objet){
